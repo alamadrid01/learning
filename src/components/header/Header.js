609e22bb534c00e59633895c3d0e8962c565110d
oyinkansola <adebayoalameen88@gmail.com>
2022-10-28 22:02:01 +0100

    Updating the header component media query and styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './header.scss';
 import arrow from './arrow.svg';
+import dot from './dot.svg';
 
 function Header() {
+  const [matches, setMatches] = useState(
+    window.matchMedia("(max-width: 500px)").matches)
+
+  useEffect(() => {
+    window
+    .matchMedia("(max-width: 500px)")
+    .addEventListener('change', e => setMatches( e.matches ));
+  }, [])
   return (
     <div>
       <header>
         <div className="arrow">
-            <img src={arrow} alt="arrow" />
+          {
+            matches ? <img src={dot} alt="arrow" /> : <img src={arrow} alt="arrow" />
+          }
+            
         </div>
       </header>
     </div>
