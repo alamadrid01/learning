df9b89287e2ba2e033a2b1e436029f08ad621a24
oyinkansola <adebayoalameen88@gmail.com>
2022-10-28 18:08:36 +0100

    Styling the link-tree component

--- src/components/Link-tree/Link.js
+++ src/components/Link-tree/Link.js
@@ -7,27 +7,27 @@ function Link() {
       <div className="linkTree">
         <div className="tree">
           <a id="btn__zuri" href="https://training.zuri.team/">
-            Link
+            Zuri Team
           </a>
         </div>
         <div className="tree">
-          <a id="btn__zuri" href="https://training.zuri.team/">
-            Link
+          <a id="books" href="http://books.zuri.team/">
+            Zuri Books
           </a>
         </div>
         <div className="tree">
-          <a id="btn__zuri" href="https://training.zuri.team/">
-            Link
+          <a id="book__python" href="https://books.zuri.team/python-for-beginners?ref_id=<alamadrid>">
+            Zuri Python Book
           </a>
         </div>
         <div className="tree">
-          <a id="btn__zuri" href="https://training.zuri.team/">
-            Link
+          <a id="pitch" href="https://background.zuri.team/">
+            Background Check for coders
           </a>
         </div>
         <div className="tree">
-          <a id="btn__zuri" href="https://training.zuri.team/">
-            Link
+          <a id="book_design" href="https://books.zuri.team/design-rules">
+            Zuri Book Design
           </a>
         </div>
       </div>
