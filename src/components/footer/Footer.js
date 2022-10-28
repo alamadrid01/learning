d9daeda800d3b2b5834247eed5ab675f68145364
oyinkansola <adebayoalameen88@gmail.com>
2022-10-28 21:09:21 +0100

    Styling and updating the footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import './footer.scss'
+import React from 'react';
+import './footer.scss';
+import zuri from './zuri.svg';
+import ingressive from './ingressive.svg';
+
 
 function Footer() {
   return (
     <div>
       <footer>
-        Footer
+        <div className="text">
+            <img src={zuri} alt="zuri-image" />
+        </div>
+        <div className="text">
+            <span>HNG internship 9 Frontend Task </span>
+        </div>
+        <div className="text">
+        <img src={ingressive} alt="zuri-image" />
+        </div>
       </footer>
     </div>
   )
