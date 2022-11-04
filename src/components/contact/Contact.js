a038fd5262ed1fa82b994daadda45a0c87d36384
oyinkansola <adebayoalameen88@gmail.com>
2022-11-04 21:56:52 +0100

    style: Updated the styling of the contact component

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -13,12 +13,29 @@ export class Contact extends Component {
       }
     }
 
-    handler =(e) =>{
-      e.preventDefault();
-
+    firstHandler =(e) =>{
       this.setState({
-        [this.target.name]: e.targe.value
+        firstName: e.target.value
       })
+  }
+    secondHandler =(e) =>{
+      this.setState({
+        lastName: e.target.value
+      })
+  }
+    thirdHandler =(e) =>{
+      this.setState({
+        email: e.target.value
+      })
+  }
+    fourthHandler =(e) =>{
+      this.setState({
+        message: e.target.value
+      })
+  }
+  submitHandler = (e) =>{
+    e.preventDefault()
+    alert('Message successfully sent')
   }
 
   render() {
@@ -29,36 +46,35 @@ export class Contact extends Component {
         <div className="contact">
             <h1>Contact me</h1>
             <p>Hi there, contact me to ask me about anything you have in mind.</p>
-            <form>
+            <form onSubmit={this.submitHandler}>
               <div className="b">
-                <div className="a">
+                <div className="a c">
                   <label>First name</label>
-                  <input type="text" placeholder='Enter your first name' value={firstName}  onChange={this.handler} />
+                  <input autoFocus type="text" placeholder='Enter your first name' value={firstName}  onChange={this.firstHandler} />
                 </div>
 
-                <div className="a">
+                <div className="a d">
                   <label>Last name</label>
-                  <input type="text" placeholder='Enter your last name' value={lastName} onChange={this.handler} />
+                  <input type="text" placeholder='Enter your last name' value={lastName} onChange={this.secondHandler} />
                 </div>
               </div>
 
               <div className="a">
                 <label>Email</label>
-                <input type="email" placeholder='yourname@email.com' value={email} onChange={this.handler} />
+                <input type="email" placeholder='yourname@email.com' value={email} onChange={this.thirdHandler} />
               </div>
 
               <div className="a">
                 <label>Message</label>
-                <textarea value={message} id="" cols="30" rows="10" placeholder="Send me a message and i'll reply you as soon as possible..."></textarea>
+                <textarea value={message} onChange={this.fourthHandler} id="" cols="30" rows="7" placeholder="Send me a message and i'll reply you as soon as possible..."></textarea>
               </div>
 
-              <div className="a">
-                <input type="checkbox" name="" id="" />
-                <label>You agree to providing your data Adebayo Al-ameen who may contact you</label>
+              <div className="a e">
+                <input required type="checkbox" name="" id="" />
+                <label className='check'>You agree to providing your data Adebayo Al-ameen who may contact you</label>
               </div>
 
-              <button id='btn__submit' type='submit'>Send message</button>
-
+              <button  id='btn__submit' type='submit'>Send message</button>
             </form>
         </div>
       </div>
